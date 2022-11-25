c26b0932cb5659c75ecdda4722caa0157dc242bb
Corina Suletea <corinasulete@gmail.com>
2022-11-25 16:50:41 +0200

    ministry brands - group slider

--- src/Placeholder/GroupSliderPlaceholder.php
+++ src/Placeholder/GroupSliderPlaceholder.php
@@ -12,25 +12,25 @@ class GroupSliderPlaceholder extends PlaceholderAbstract
     public function echoValue(ContextInterface $context, ContentPlaceholder $placeholder)
     {
         $options = [
-            'show_image'                      => true,
-            'show_category'                   => true,
-            'show_group'                      => true,
+            'show_image'                      => false,
+            'show_category'                   => false,
+            'show_group'                      => false,
             'category'                        => 'all',
             'group'                           => 'all',
-            'show_preview'                    => true,
+            'show_preview'                    => false,
             'detail_page_button_text'         => false,
             'detail_page'                     => false,
             'howmany'                         => 9,
             'column_count'                    => 3,
-            'show_pagination'                 => true,
-            'show_images'                     => true,
-            'show_day'                        => true,
-            'show_times'                      => true,
-            'show_status'                     => true,
-            'show_childcare'                  => true,
-            'show_resourcelink'               => true,
+            'show_pagination'                 => false,
+            'show_images'                     => false,
+            'show_day'                        => false,
+            'show_times'                      => false,
+            'show_status'                     => false,
+            'show_childcare'                  => false,
+            'show_resourcelink'               => false,
             'howmany_show'                    => 3,
-            'show_arrows'                     => true,
+            'show_arrows'                     => false
         ];
 
         $settings = array_merge($options, $placeholder->getAttributes());
@@ -46,23 +46,23 @@ class GroupSliderPlaceholder extends PlaceholderAbstract
             'order'         => 'recent',
             'emailencode'   => 'no',
             'howmany'       => $howmany,
-            'page'          => isset($_GET['ekklesia360_group_slider_page']) ? $_GET['ekklesia360_group_list_page'] : 1,
+            'page'          => isset($_GET['brz-groupSlider_page']) ? $_GET['ekklesia360_group_list_page'] : 1,
             'find_category' => $category == 'all' ? '' : $category,
             'find_group'    => $group == 'all' ? '' : $group,
             'show'          => "__starttime format='g:ia'__",
-            'show'          => "__endtime format='g:ia'__",
+            'show'          => "__endtime format='g:ia'__"
         ]);
         ?>
 
-        <div class="ekklesia360_group_slider_wrap" data-showarrows="<?= $show_arrows ?>"
+        <div class="brz-groupSlider_wrap" data-showarrows="<?= $show_arrows ?>"
              data-showpagination="<?= $show_pagination ?>">
 
             <?php //output
             if (!empty($content['show'])) {
                 ?>
 
-                <div class="swiper-container ekklesia360_group_slider" data-howmanyshow="<?= $howmany_show ?>">
-                    <div class="swiper-wrapper">
+                <div class="brz-groupSlider-swiper-container" data-howmanyshow="<?= $howmany_show ?>">
+                    <div class="brz-groupSlider-swiper-wrapper" data-show="<?= $column_count?>">
                         <?php
                         foreach ($content['show'] as $key => $item) {
                             //remove 12am
@@ -76,71 +76,73 @@ class GroupSliderPlaceholder extends PlaceholderAbstract
                             $item['group'] = str_replace("Site Group, ", "", $item['group']);
                             $item['group'] = str_replace("Site Group", "", $item['group']);
 
-                            echo "<article class=\"swiper-slide\">";
-                            echo "<div class=\"info\">";
+                            echo "<article class=\"brz-groupSlider-swiper-slide\">";
+                            echo "<div class=\"brz-groupSlider-info\">";
                             if ($show_images && $item['imageurl']) {
                                 if ($detail_url) echo "<a href=\"{$detail_url}?ekklesia360_group_slug={$item['slug']}\">";
-                                echo "<div class=\"image\"><img src=\"{$item['imageurl']}\" alt=\"\" /></div>";
+                                echo "<div class=\"brz-groupSlider-image\"><img src=\"{$item['imageurl']}\" alt=\"\" /></div>";
                                 if ($detail_url) echo "</a>";
                             }
 
-                            echo "<h4 class=\"ekklesia360_group_slider_heading\">";
+                            echo "<h4 class=\"brz-groupSlider_heading\">";
                             if ($detail_url) echo "<a href=\"{$detail_url}?ekklesia360_group_slug={$item['slug']}\">";
                             echo "{$item['name']}";
                             if ($detail_url) echo "</a>";
                             echo "</h4>";
 
                             if ($show_day && $item['dayoftheweek']) {
-                                echo "<h6 class=\"ekklesia360_group_slider_meta\">Meeting Day: {$item['dayoftheweek']}</h6>";
+                                echo "<h6 class=\"brz-groupSlider_meta\">Meeting Day: {$item['dayoftheweek']}</h6>";
                             }
                             if ($show_times && ($item['starttime'] || $item['endtime'])) {
-                                echo "<h6 class=\"ekklesia360_group_slider_meta\">Meeting Times: ";
+                                echo "<h6 class=\"brz-groupSlider_meta\">Meeting Times: ";
                                 if ($item['starttime']) echo "{$item['starttime']}";
                                 if ($item['endtime']) echo " - {$item['endtime']}";
                                 echo "</h6>";
                             }
                             if ($show_category && $item['category']) {
-                                echo "<h6 class=\"ekklesia360_group_slider_meta\">Category: {$item['category']}</h6>";
+                                echo "<h6 class=\"brz-groupSlider_meta\">Category: {$item['category']}</h6>";
                             }
                             if ($show_group && $item['group']) {
-                                echo "<h6 class=\"ekklesia360_group_slider_meta\">Group: {$item['group']}</h6>";
+                                echo "<h6 class=\"brz-groupSlider_meta\">Group: {$item['group']}</h6>";
                             }
                             if ($show_status && $item['groupstatus']) {
-                                echo "<h6 class=\"ekklesia360_group_slider_meta\">Status: {$item['groupstatus']}</h6>";
+                                echo "<h6 class=\"brz-groupSlider_meta\">Status: {$item['groupstatus']}</h6>";
                             }
                             if ($show_childcare) {
                                 $childcare = "No";
                                 if ($item['childcareprovided']) {
                                     $childcare = "Yes";
                                 }
-                                echo "<h6 class=\"ekklesia360_group_slider_meta\">Childcare Provided: {$childcare}</h6>";
+                                echo "<h6 class=\"brz-groupSlider_meta\">Childcare Provided: {$childcare}</h6>";
                             }
                             if ($show_resourcelink && $item['resourcelink']) {
                                 $resource_target = "";
                                 if ($item['iflinknewwindow']) {
                                     $resource_target = " target=\"_blank\"";
                                 }
-                                echo "<h6 class=\"ekklesia360_group_slider_meta\">Resource: <a href=\"{$item['resourcelink']}\" {$resource_target}>{$item['resourcelink']}</a></h6>";
+                                echo "<h6 class=\"brz-groupSlider_meta\">Resource: <a href=\"{$item['resourcelink']}\" {$resource_target}>{$item['resourcelink']}</a></h6>";
                             }
                             if ($show_preview && $item['description']) {
                                 $item['description'] = substr($item['description'], 0, 110) . " ...";
-                                echo "<p class=\"ekklesia360_group_slider_preview\">{$item['description']}</p>";
+                                echo "<p class=\"brz-groupSlider_preview\">{$item['description']}</p>";
                             }
                             if ($detail_url && $detail_page_button_text) {
-                                echo "<p class=\"ekklesia360_group_slider_detail_button\"><a href=\"{$detail_url}?ekklesia360_group_slug={$item['slug']}\" class=\"brz-button-link brz-button brz-size-sm\"><span class=\"brz-button-text\">{$detail_page_button_text}</span></a></p>";
+                                echo "<p class=\"brz-groupSlider_detail_button\"><a href=\"{$detail_url}?ekklesia360_group_slug={$item['slug']}\" class=\"brz-button-link brz-button brz-size-sm\"><span class=\"brz-button-text\">{$detail_page_button_text}</span></a></p>";
                             }
                             echo "</div>";
                             echo "</article>";
+        
                         }
                         ?>
                     </div>
                     <?php if ($show_pagination): ?>
-                        <div class="swiper-pagination"></div>
+                        <div class="brz-groupSlider-swiper-pagination"></div>
                     <?php endif; ?>
                 </div>
                 <?php if ($show_arrows): ?>
-                    <div class="swiper-arrow-prev"><i class="fas fa-angle-left"></i></div>
-                    <div class="swiper-arrow-next"><i class="fas fa-angle-right"></i></div>
+                    <div class="brz-swiper-arrow brz-swiper-arrow_prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512" class="brz-icon-svg align-[initial]" data-type="fa" data-name="angle-left"><path d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg></i></div>
+                    <div class="brz-swiper-arrow brz-swiper-arrow_next"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512" class="brz-icon-svg align-[initial]" data-type="fa" data-name="angle-right"><path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg>
+                </div>
                 <?php endif; ?>
                 <?php
             } //no output
