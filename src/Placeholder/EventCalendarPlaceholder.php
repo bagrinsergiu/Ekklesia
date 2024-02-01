16bbf089a0a7412d766444548b1dc4042c229300
DanielGherjavca <danniel.gherjavca@gmail.com>
2024-02-01 20:56:59 +0200

    feat(ministrybrands): event calendar - removed bell icon

--- src/Placeholder/EventCalendarPlaceholder.php
+++ src/Placeholder/EventCalendarPlaceholder.php
@@ -175,8 +175,7 @@ class EventCalendarPlaceholder extends PlaceholderAbstract
                     $month_format_month,
                     $month_format_year,
                     $events[$month_format],
-                    $detail_url,
-                    $showSubscribeToCalendarButton
+                    $detail_url
                 );
 
             }//end if
@@ -198,8 +197,7 @@ class EventCalendarPlaceholder extends PlaceholderAbstract
         $month,
         $year,
         $events = null,
-        $detail_url = null,
-        $showSubscribeToCalendarButton = false
+        $detail_url = null
     ) {
 
         /* draw table */
@@ -249,10 +247,6 @@ class EventCalendarPlaceholder extends PlaceholderAbstract
                     }
                     $calendar .= "<li>";
                     $calendar .= "<div class=\"brz-eventCalendar-title\">";
-
-                    if ($showSubscribeToCalendarButton) {
-                        $calendar .= '<span class="brz-eventCalendar-title__subscribe__icon"><a href="?mc-subscribe='.$v["occurrenceid"].'"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="brz-icon-svg align-[initial]"><path d="M224 512c35.32 0 63.97-28.65 63.97-64H160.03c0 35.35 28.65 64 63.97 64zm215.39-149.71c-19.32-20.76-55.47-51.99-55.47-154.29 0-77.7-54.48-139.9-127.94-155.16V32c0-17.67-14.32-32-31.98-32s-31.98 14.33-31.98 32v20.84C118.56 68.1 64.08 130.3 64.08 208c0 102.3-36.15 133.53-55.47 154.29-6 6.45-8.66 14.16-8.61 21.71.11 16.4 12.98 32 32.1 32h383.8c19.12 0 32-15.6 32.1-32 .05-7.55-2.61-15.27-8.61-21.71z"></path></svg></a></span>';
-                    }
 
                     if ($detail_url) {
                         $calendar .= "<a href=\"{$detail_url}?mc-slug={$v['slug']}\" title=\"{$v["title"]}\">";
--- src/Placeholder/EventDetailPlaceholder.php
+++ src/Placeholder/EventDetailPlaceholder.php
@@ -32,7 +32,8 @@ class EventDetailPlaceholder extends PlaceholderAbstract
             'show_description'              => false,
             'events_recent'                 => false,
             'previous_page'                 => false,
-            'showSubscribeToCalendarButton' => true,
+            'show_subscribe_to_event'=> false,
+            'subscribe_to_event_button_text' => ''
         ];
 
         $settings = array_merge($options, $placeholder->getAttributes());
@@ -179,9 +180,10 @@ class EventDetailPlaceholder extends PlaceholderAbstract
                     Previous Page</div>';
                 }
 
-                if ($showSubscribeToCalendarButton) {
-                    echo '<span class="brz-eventCalendar-title__subscribe__icon"><a href="?mc-subscribe='.$item['occurrenceid'].'"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="brz-icon-svg align-[initial]"><path d="M224 512c35.32 0 63.97-28.65 63.97-64H160.03c0 35.35 28.65 64 63.97 64zm215.39-149.71c-19.32-20.76-55.47-51.99-55.47-154.29 0-77.7-54.48-139.9-127.94-155.16V32c0-17.67-14.32-32-31.98-32s-31.98 14.33-31.98 32v20.84C118.56 68.1 64.08 130.3 64.08 208c0 102.3-36.15 133.53-55.47 154.29-6 6.45-8.66 14.16-8.61 21.71.11 16.4 12.98 32 32.1 32h383.8c19.12 0 32-15.6 32.1-32 .05-7.55-2.61-15.27-8.61-21.71z"></path></svg></a></span>';
+                if($show_subscribe_to_event ){
+                    echo '<div class="brz-eventDetail__item--subscribe-event"><a href="?mc-subscribe=' . $item["occurrenceid"] . '">' . $subscribe_to_event_button_text . '</a></div>';
                 }
+
                 ?>
             </div>
             <?php
