10b964ddcd7223d9415e9e4192326aa8f36eba87
Viorel <eremiaviorel0@gmail.com>
2024-02-03 14:58:45 +0200

    Review aritlces placehodlers

--- src/Placeholder/ArticleDetailPlaceholder.php
+++ src/Placeholder/ArticleDetailPlaceholder.php
@@ -9,7 +9,7 @@ class ArticleDetailPlaceholder extends PlaceholderAbstract
 {
     protected $name = 'ekk_article_detail';
 
-    public function echoValue( ContextInterface $context, ContentPlaceholder $placeholder )
+    public function echoValue(ContextInterface $context, ContentPlaceholder $placeholder)
     {
         $options = [
             'show_image'                => false,
@@ -31,158 +31,165 @@ class ArticleDetailPlaceholder extends PlaceholderAbstract
             'previous_page'             => false,
         ];
 
-        $settings = array_merge( $options, $placeholder->getAttributes() );
+        $settings = array_merge($options, $placeholder->getAttributes());
         $cms      = $this->monkCMS;
 
-        extract( $settings );
+        extract($settings);
 
-        if ( ! empty( ($_GET['mc-slug']) ) ) {
-            $slug = $_GET['mc-slug'];
-        } elseif ( $articles_recent ) {
-            $slug = $articles_recent;
-        } else {
-            $recent = $cms->get( [
+        if (!empty($_GET['mc-slug']) || $articles_recent) {
+            $content = $cms->get([
+                'module'      => 'article',
+                'display'     => 'detail',
+                'find'        => $_GET['mc-slug'] ?: $articles_recent,
+                'emailencode' => 'no',
+            ]);
+        }
+
+        if (empty($content['show'])) {
+            $recent = $cms->get([
                 'module'      => 'article',
                 'display'     => 'list',
                 'order'       => 'recent',
                 'howmany'     => 1,
                 'emailencode' => 'no',
-                'show'        => "__videoplayer fullscreen='true'__",
-                'show'        => "__audioplayer__",
-            ] );
+            ]);
 
-            $slug = isset( $recent['show'][0]['slug'] ) ? $recent['show'][0]['slug'] : '';
-        }
-
-        $content = $cms->get( [
-            'module'      => 'article',
-            'display'     => 'detail',
-            'find'        => $slug,
-            'emailencode' => 'no',
-            'show'        => "__videoplayer fullscreen='true'__",
-            'show'        => "__audioplayer__",
-        ] );
-
-        if ( empty( $content['show'] ) ) {
-            echo '<p>There is no article available.</p>';
-            return;
+            if (empty($recent['show'][0])) {
+                return;
+            }
         }
 
         $item = $content['show'];
 
         echo "<div class=\"brz-articleDetail__item\">";
 
-        if ( $show_title ) {
+        if ($show_title) {
             echo "<h2 class=\"brz-articleDetail__item--meta--title\">{$item['title']}</h2>";
         }
 
-        if ( $show_date && $item['date'] ) {
+        if ($show_date && $item['date']) {
             echo "<h6 class=\"brz-articleDetail__item--meta\">";
-                if ( $show_meta_headings ) {
-                    echo "Date: ";
-                }
-                echo "{$item['date']}";
+            if ($show_meta_headings) {
+                echo "Date: ";
+            }
+            echo "{$item['date']}";
             echo "</h6>";
         }
 
-        if ( $show_category && $item['category'] ) {
+        if ($show_category && $item['category']) {
             echo "<h6 class=\"brz-articleDetail__item--meta\">";
-                if ( $show_meta_headings ) {
-                    echo "Category: ";
-                }
-                echo "{$item['category']}";
+            if ($show_meta_headings) {
+                echo "Category: ";
+            }
+            echo "{$item['category']}";
             echo "</h6>";
         }
 
-        if ( $show_group && $item['group'] ) {
+        if ($show_group && $item['group']) {
             echo "<h6 class=\"brz-articleDetail__item--meta\">";
-                if ( $show_meta_headings ) {
-                    echo "Group: ";
-                }
-                echo "{$item['group']}";
+            if ($show_meta_headings) {
+                echo "Group: ";
+            }
+            echo "{$item['group']}";
             echo "</h6>";
         }
 
-        if ( $show_series && $item['series'] ) {
+        if ($show_series && $item['series']) {
             echo "<h6 class=\"brz-articleDetail__item--meta\">";
-                if ( $show_meta_headings ) {
-                    echo "Series: ";
-                }
-                echo "{$item['series']}";
+            if ($show_meta_headings) {
+                echo "Series: ";
+            }
+            echo "{$item['series']}";
             echo "</h6>";
         }
 
-        if ( $show_author && $item['author'] ) {
+        if ($show_author && $item['author']) {
             echo "<h6 class=\"brz-articleDetail__item--meta\">";
-                if ( $show_meta_headings ) {
-                    echo "Author: ";
-                }
-                echo "{$item['author']}";
+            if ($show_meta_headings) {
+                echo "Author: ";
+            }
+            echo "{$item['author']}";
             echo "</h6>";
         }
 
-        if ( $show_image && $item['imageurl'] && ! $show_video ) {
+        if ($show_image && $item['imageurl'] && !$show_video) {
             echo "<div class=\"brz-ministryBrands__item--media\"><img src=\"{$item['imageurl']}\" alt=\"\" /></div>";
         }
 
-        if ( $show_video ) {
-            if ( $item['videoembed'] ) {
+        if ($show_video) {
+            if ($item['videoembed']) {
                 echo "<div class=\"brz-ministryBrands__item--media\">{$item['videoembed']}</div>";
-            } elseif ( $item['videourl'] ) {
-                $videoext = pathinfo( $item['videourl'], PATHINFO_EXTENSION );
+            } elseif ($item['videourl']) {
+                $videoext = pathinfo($item['videourl'], PATHINFO_EXTENSION);
                 echo "<div class=\"brz-ministryBrands__item--media\">";
-                    echo "<video src=\"{$item['videourl']}\" controls preload=\"none\" width=\"1024\" height=\"576\" poster=\"{$item['imageurl']}\" type=\"video/{$videoext}\"><p>The Video could not be played. Please <a href=\"{$item['videourl']}\" target=\"_blank\">download it here</a>.</p></video>";
+                echo "<video src=\"{$item['videourl']}\" controls preload=\"none\" width=\"1024\" height=\"576\" poster=\"{$item['imageurl']}\" type=\"video/{$videoext}\"><p>The Video could not be played. Please <a href=\"{$item['videourl']}\" target=\"_blank\">download it here</a>.</p></video>";
                 echo "</div>";
-            } elseif ( $show_image && $item['imageurl'] ) {
+            } elseif ($show_image && $item['imageurl']) {
                 echo "<div class=\"brz-ministryBrands__item--media\">
                        <img src=\"{$item['imageurl']}\" alt=\"\" />
                     </div>";
             }
         }
 
-        if ( $show_audio && $item['audiourl'] ) {
+        if ($show_audio && $item['audiourl']) {
             echo "<div class=\"brz-articleDetail__item--media--audio\">";
-                echo "<audio src=\"{$item['audiourl']}\" controls preload=\"none\"></audio>";
+            echo "<audio src=\"{$item['audiourl']}\" controls preload=\"none\"></audio>";
             echo "</div>";
         }
 
-        if ( $show_media_links_video || $show_media_links_audio || $show_media_links_download || $show_media_links_notes ) {
+        if ($show_media_links_video || $show_media_links_audio || $show_media_links_download || $show_media_links_notes) {
             echo "<ul class=\"brz-articleDetail__item--media\">";
 
-                if ( $show_media_links_video && ! empty( $item['videoplayer'] ) ) {
-                    if ( strpos( $item['videoplayer'], 'class' ) ) {
-                        $item['videoplayer'] = str_replace( "Launch Player", "Watch", $item['videoplayer'] );
-                        $item['videoplayer'] = str_replace( "Watch Video", "Watch", $item['videoplayer'] );
-                        $item['videoplayer'] = str_replace( 'mcms_videoplayer', 'mcms_videoplayer brz-button-link brz-button brz-size-sm', $item['videoplayer'] );
-                    } else {
-                        $item['videoplayer'] = str_replace( ">Launch Player", " class=\"brz-button-link brz-button brz-size-sm\">Watch", $item['videoplayer'] );
-                        $item['videoplayer'] = str_replace( ">Watch Video", " class=\"brz-button-link brz-button brz-size-sm\">Watch", $item['videoplayer'] );
-                    }
-
-                    echo "<li class=\"brz-ministryBrands__item--meta--links\">{$item['videoplayer']}</li>";
+            if ($show_media_links_video && !empty($item['videoplayer'])) {
+                if (strpos($item['videoplayer'], 'class')) {
+                    $item['videoplayer'] = str_replace("Launch Player", "Watch", $item['videoplayer']);
+                    $item['videoplayer'] = str_replace("Watch Video", "Watch", $item['videoplayer']);
+                    $item['videoplayer'] = str_replace(
+                        'mcms_videoplayer',
+                        'mcms_videoplayer brz-button-link brz-button brz-size-sm',
+                        $item['videoplayer']
+                    );
+                } else {
+                    $item['videoplayer'] = str_replace(
+                        ">Launch Player",
+                        " class=\"brz-button-link brz-button brz-size-sm\">Watch",
+                        $item['videoplayer']
+                    );
+                    $item['videoplayer'] = str_replace(
+                        ">Watch Video",
+                        " class=\"brz-button-link brz-button brz-size-sm\">Watch",
+                        $item['videoplayer']
+                    );
                 }
 
-                if ( $show_media_links_audio && ! empty( $item['audioplayer'] ) ) {
-                    $item['audioplayer'] = str_replace( "Launch Player", "Listen", $item['audioplayer'] );
-                    $item['audioplayer'] = str_replace( 'mcms_audioplayer', 'mcms_audioplayer brz-button-link brz-button brz-size-sm', $item['audioplayer'] );
-                    echo "<li class=\"brz-ministryBrands__item--meta--links\">{$item['audioplayer']}</li>";
-                }
+                echo "<li class=\"brz-ministryBrands__item--meta--links\">{$item['videoplayer']}</li>";
+            }
 
-                if ( $show_media_links_download && ! empty( $item['audio'] ) ) {
-                    echo "<li class=\"brz-ministryBrands__item--meta--links\"><a href=\"{$item['audio']}\" class=\"brz-button-link brz-button brz-size-sm\">Download Audio</a></li>";
-                }
+            if ($show_media_links_audio && !empty($item['audioplayer'])) {
+                $item['audioplayer'] = str_replace("Launch Player", "Listen", $item['audioplayer']);
+                $item['audioplayer'] = str_replace(
+                    'mcms_audioplayer',
+                    'mcms_audioplayer brz-button-link brz-button brz-size-sm',
+                    $item['audioplayer']
+                );
+                echo "<li class=\"brz-ministryBrands__item--meta--links\">{$item['audioplayer']}</li>";
+            }
 
-                if ( $show_media_links_notes && ! empty( $item['notes'] ) ) {
-                    echo "<li class=\"brz-ministryBrands__item--meta--links\"><a href=\"{$item['notes']}\" class=\"brz-button-link brz-button brz-size-sm\" target=\"_blank\">Notes</a></li>";
-                }
+            if ($show_media_links_download && !empty($item['audio'])) {
+                echo "<li class=\"brz-ministryBrands__item--meta--links\"><a href=\"{$item['audio']}\" class=\"brz-button-link brz-button brz-size-sm\">Download Audio</a></li>";
+            }
+
+            if ($show_media_links_notes && !empty($item['notes'])) {
+                echo "<li class=\"brz-ministryBrands__item--meta--links\"><a href=\"{$item['notes']}\" class=\"brz-button-link brz-button brz-size-sm\" target=\"_blank\">Notes</a></li>";
+            }
             echo "</ul>";
         }
 
-        if ( $show_content && ! empty( $item['text'] ) ) {
+        if ($show_content && !empty($item['text'])) {
             echo "<div class=\"brz-articleDetail__item--content\">{$item['text']}</div>";
         }
 
-        if ( $previous_page ) {
+        if ($previous_page) {
             echo '<div class="brz-ministryBrands__item--meta--links brz-ministryBrands__item--meta--links--previous">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512" class="brz-icon-svg align-[initial]"><path d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg>
             Previous Page</div>';
--- src/Placeholder/ArticleFeaturedPlaceholder.php
+++ src/Placeholder/ArticleFeaturedPlaceholder.php
@@ -8,45 +8,46 @@ use BrizyPlaceholders\ContextInterface;
 class ArticleFeaturedPlaceholder extends PlaceholderAbstract
 {
     protected $name = 'ekk_article_featured';
+
     public function echoValue(ContextInterface $context, ContentPlaceholder $placeholder)
     {
         $options = [
-            'show_image' => false,
-            'show_video' => false,
-            'show_audio' => false,
-            'show_media_links' => false,
-            'show_title' => false,
-            'show_date' => false,
-            'show_category' => false,
-            'show_group' => false,
-            'show_series' => false,
-            'show_author' => false,
-            'show_meta_headings' => false,
-            'show_preview' => false,
-            'show_content' => false,
+            'show_image'              => false,
+            'show_video'              => false,
+            'show_audio'              => false,
+            'show_media_links'        => false,
+            'show_title'              => false,
+            'show_date'               => false,
+            'show_category'           => false,
+            'show_group'              => false,
+            'show_series'             => false,
+            'show_author'             => false,
+            'show_meta_headings'      => false,
+            'show_preview'            => false,
+            'show_content'            => false,
             'detail_page_button_text' => false,
-            'detail_page' => false,
-            'article_slug' => false,
-            'recentArticles' => '',
-            'features' => '',
-            'nonfeatures' => '',
-            'series' => 'all',
-            'category' => 'all',
-            'group' => 'all',
-            'show_latest_articles' => false,
+            'detail_page'             => false,
+            'article_slug'            => false,
+            'recentArticles'          => '',
+            'features'                => '',
+            'nonfeatures'             => '',
+            'series'                  => 'all',
+            'category'                => 'all',
+            'group'                   => 'all',
+            'show_latest_articles'    => false,
         ];
 
         $settings = array_merge($options, $placeholder->getAttributes());
 
         extract($settings);
 
-        $cms = $this->monkCMS;
+        $cms            = $this->monkCMS;
         $recentArticles = $settings['recentArticles'] != '' ? $settings['recentArticles'] : '';
-        $category = $settings['category'] != 'all' ? $settings['category'] : '';
-        $group = $settings['group'] != 'all' ? $settings['group'] : '';
-        $series = $settings['series'] != 'all' ? $settings['series'] : '';
-        $detail_url = $detail_page ? $this->replacer->replacePlaceholders(urldecode($detail_page), $context) : false;
-        $slug = false;
+        $category       = $settings['category'] != 'all' ? $settings['category'] : '';
+        $group          = $settings['group'] != 'all' ? $settings['group'] : '';
+        $series         = $settings['series'] != 'all' ? $settings['series'] : '';
+        $detail_url     = $detail_page ? $this->replacer->replacePlaceholders(urldecode($detail_page), $context) : false;
+        $slug           = false;
 
         if ($features) {
             $nonfeatures = '';
@@ -57,19 +58,18 @@ class ArticleFeaturedPlaceholder extends PlaceholderAbstract
         //make content
         if ($show_latest_articles) {
             $content1 = $cms->get([
-                'module' => 'article',
-                'display' => 'list',
-                'order' => 'recent',
-                'howmany' => 1,
+                'module'        => 'article',
+                'display'       => 'list',
+                'order'         => 'recent',
+                'howmany'       => 1,
                 'find_category' => $category,
-                'find_group' => $group,
-                'features' => $features,
-                'nonfeatures' => $nonfeatures,
-                'emailencode' => 'no',
+                'find_group'    => $group,
+                'features'      => $features,
+                'nonfeatures'   => $nonfeatures,
+                'emailencode'   => 'no',
             ]);
 
             $content = empty($content1['show'][0]) ? [] : $content1['show'][0];
-
 
         } else {
             if ($article_slug) {
@@ -81,75 +81,82 @@ class ArticleFeaturedPlaceholder extends PlaceholderAbstract
 
         if ($slug) {
             $content = $cms->get([
-                'module' => 'article',
-                'display' => 'detail',
-                'find' => $slug,
+                'module'      => 'article',
+                'display'     => 'detail',
+                'find'        => $slug,
                 'emailencode' => 'no',
-                'show' => "__videoplayer fullscreen='true'__",
-                'show' => "__audioplayer__",
             ])['show'];
         }
         ?>
 
 
         <?php //output
-                if (isset($content) && count($content) > 0) {
-                    $item = $content;
-                    ?>
+        if (isset($content) && count($content) > 0) {
+            $item = $content;
+            ?>
             <?php
             echo "<article>";
             echo "<div class=\"brz-articleFeatured__item\">";
             if ($show_title) {
                 echo "<h2 class=\"brz-articleFeatured__item--meta--title\">";
-                if ($detail_url)
+                if ($detail_url) {
                     echo "<a href=\"{$detail_url}?mc-slug={$item['slug']}\">";
+                }
                 echo "{$item['title']}";
-                if ($detail_url)
+                if ($detail_url) {
                     echo "</a>";
+                }
                 echo "</h2>";
             }
             if ($show_date && $item['date']) {
                 echo "<h6 class=\"brz-articleFeatured__item--meta\">";
-                if ($show_meta_headings)
+                if ($show_meta_headings) {
                     echo "Date: ";
+                }
                 echo "{$item['date']}";
                 echo "</h6>";
             }
             if ($show_category && $item['category']) {
                 echo "<h6 class=\"brz-articleFeatured__item--meta\">";
-                if ($show_meta_headings)
+                if ($show_meta_headings) {
                     echo "Category: ";
+                }
                 echo "{$item['category']}";
                 echo "</h6>";
             }
             if ($show_group && $item['group']) {
                 echo "<h6 class=\"brz-articleFeatured__item--meta\">";
-                if ($show_meta_headings)
+                if ($show_meta_headings) {
                     echo "Group: ";
+                }
                 echo "{$item['group']}";
                 echo "</h6>";
             }
             if ($show_series && $item['series']) {
                 echo "<h6 class=\"brz-articleFeatured__item--meta\">";
-                if ($show_meta_headings)
+                if ($show_meta_headings) {
                     echo "Series: ";
+                }
                 echo "{$item['series']}";
                 echo "</h6>";
             }
             if ($show_author && $item['author']) {
                 echo "<h6 class=\"brz-articleFeatured__item--meta\">";
-                if ($show_meta_headings)
+                if ($show_meta_headings) {
                     echo "Author: ";
+                }
                 echo "{$item['author']}";
                 echo "</h6>";
             }
             if ($show_image && $item['imageurl'] && !$show_video) {
                 echo "<div class=\"brz-ministryBrands__item--media\">";
-                if ($detail_url)
+                if ($detail_url) {
                     echo "<a href=\"{$detail_url}?mc-slug={$item['slug']}\">";
+                }
                 echo "<img src=\"{$item['imageurl']}\" alt=\"\" />";
-                if ($detail_url)
+                if ($detail_url) {
                     echo "</a>";
+                }
                 echo "</div>";
             }
             if ($show_video) {
@@ -162,11 +169,13 @@ class ArticleFeaturedPlaceholder extends PlaceholderAbstract
                     echo "</div>";
                 } elseif ($show_image && $item['imageurl']) {
                     echo "<div class=\"image\">";
-                    if ($detail_url)
+                    if ($detail_url) {
                         echo "<a href=\"{$detail_url}?nc-slug={$item['slug']}\">";
+                    }
                     echo "<img src=\"{$item['imageurl']}\" alt=\"\" />";
-                    if ($detail_url)
+                    if ($detail_url) {
                         echo "</a>";
+                    }
                     echo "</div>";
                 }
             }
@@ -178,11 +187,19 @@ class ArticleFeaturedPlaceholder extends PlaceholderAbstract
             if ($show_media_links) {
                 echo "<ul class=\"brz-articleFeatured__item--media--links\">";
                 if ($item['videoplayer']) {
-                    $item['videoplayer'] = preg_replace('/<a(.+?)>.+?<\/a>/i', '<a$1><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="brz-icon-svg align-[initial]"><path d="M528 0H48C21.5 0 0 21.5 0 48v320c0 26.5 21.5 48 48 48h192l-16 48h-72c-13.3 0-24 10.7-24 24s10.7 24 24 24h272c13.3 0 24-10.7 24-24s-10.7-24-24-24h-72l-16-48h192c26.5 0 48-21.5 48-48V48c0-26.5-21.5-48-48-48zm-16 352H64V64h448v288z"></path></svg></a>', $item['videoplayer']);
+                    $item['videoplayer'] = preg_replace(
+                        '/<a(.+?)>.+?<\/a>/i',
+                        '<a$1><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="brz-icon-svg align-[initial]"><path d="M528 0H48C21.5 0 0 21.5 0 48v320c0 26.5 21.5 48 48 48h192l-16 48h-72c-13.3 0-24 10.7-24 24s10.7 24 24 24h272c13.3 0 24-10.7 24-24s-10.7-24-24-24h-72l-16-48h192c26.5 0 48-21.5 48-48V48c0-26.5-21.5-48-48-48zm-16 352H64V64h448v288z"></path></svg></a>',
+                        $item['videoplayer']
+                    );
                     echo "<li class=\"brz-ministryBrands__item--meta--links\">{$item['videoplayer']}</li>";
                 }
                 if ($item['audioplayer']) {
-                    $item['audioplayer'] = preg_replace('/<a(.+?)>.+?<\/a>/i', '<a$1><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="brz-icon-svg align-[initial]"><path d="M215.03 71.05L126.06 160H24c-13.26 0-24 10.74-24 24v144c0 13.25 10.74 24 24 24h102.06l88.97 88.95c15.03 15.03 40.97 4.47 40.97-16.97V88.02c0-21.46-25.96-31.98-40.97-16.97zm233.32-51.08c-11.17-7.33-26.18-4.24-33.51 6.95-7.34 11.17-4.22 26.18 6.95 33.51 66.27 43.49 105.82 116.6 105.82 195.58 0 78.98-39.55 152.09-105.82 195.58-11.17 7.32-14.29 22.34-6.95 33.5 7.04 10.71 21.93 14.56 33.51 6.95C528.27 439.58 576 351.33 576 256S528.27 72.43 448.35 19.97zM480 256c0-63.53-32.06-121.94-85.77-156.24-11.19-7.14-26.03-3.82-33.12 7.46s-3.78 26.21 7.41 33.36C408.27 165.97 432 209.11 432 256s-23.73 90.03-63.48 115.42c-11.19 7.14-14.5 22.07-7.41 33.36 6.51 10.36 21.12 15.14 33.12 7.46C447.94 377.94 480 319.54 480 256zm-141.77-76.87c-11.58-6.33-26.19-2.16-32.61 9.45-6.39 11.61-2.16 26.2 9.45 32.61C327.98 228.28 336 241.63 336 256c0 14.38-8.02 27.72-20.92 34.81-11.61 6.41-15.84 21-9.45 32.61 6.43 11.66 21.05 15.8 32.61 9.45 28.23-15.55 45.77-45 45.77-76.88s-17.54-61.32-45.78-76.86z"></path></svg></a>', $item['audioplayer']);
+                    $item['audioplayer'] = preg_replace(
+                        '/<a(.+?)>.+?<\/a>/i',
+                        '<a$1><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="brz-icon-svg align-[initial]"><path d="M215.03 71.05L126.06 160H24c-13.26 0-24 10.74-24 24v144c0 13.25 10.74 24 24 24h102.06l88.97 88.95c15.03 15.03 40.97 4.47 40.97-16.97V88.02c0-21.46-25.96-31.98-40.97-16.97zm233.32-51.08c-11.17-7.33-26.18-4.24-33.51 6.95-7.34 11.17-4.22 26.18 6.95 33.51 66.27 43.49 105.82 116.6 105.82 195.58 0 78.98-39.55 152.09-105.82 195.58-11.17 7.32-14.29 22.34-6.95 33.5 7.04 10.71 21.93 14.56 33.51 6.95C528.27 439.58 576 351.33 576 256S528.27 72.43 448.35 19.97zM480 256c0-63.53-32.06-121.94-85.77-156.24-11.19-7.14-26.03-3.82-33.12 7.46s-3.78 26.21 7.41 33.36C408.27 165.97 432 209.11 432 256s-23.73 90.03-63.48 115.42c-11.19 7.14-14.5 22.07-7.41 33.36 6.51 10.36 21.12 15.14 33.12 7.46C447.94 377.94 480 319.54 480 256zm-141.77-76.87c-11.58-6.33-26.19-2.16-32.61 9.45-6.39 11.61-2.16 26.2 9.45 32.61C327.98 228.28 336 241.63 336 256c0 14.38-8.02 27.72-20.92 34.81-11.61 6.41-15.84 21-9.45 32.61 6.43 11.66 21.05 15.8 32.61 9.45 28.23-15.55 45.77-45 45.77-76.88s-17.54-61.32-45.78-76.86z"></path></svg></a>',
+                        $item['audioplayer']
+                    );
                     echo "<li class=\"brz-ministryBrands__item--meta--links\">{$item['audioplayer']}</li>";
                 }
                 if ($item['notes']) {
@@ -191,7 +208,7 @@ class ArticleFeaturedPlaceholder extends PlaceholderAbstract
                 echo "</ul>";
             }
             if ($show_preview && $item['preview']) {
-                $item['preview'] = substr($item['preview'], 0, 110) . " ...";
+                $item['preview'] = substr($item['preview'], 0, 110)." ...";
                 echo "<p class=\"brz-articleFeatured__item--meta--preview\"><span>{$item['preview']}</span></p>";
             }
             if ($show_content && $item['text']) {
@@ -202,8 +219,8 @@ class ArticleFeaturedPlaceholder extends PlaceholderAbstract
             }
             echo "</div>";
             echo "</article>";
-                ?>
-        <?php
+            ?>
+            <?php
         } //no output
         else {
             ?>
@@ -211,7 +228,7 @@ class ArticleFeaturedPlaceholder extends PlaceholderAbstract
 
             <?php
         }
-    ?>
-    <?php
+        ?>
+        <?php
     }
 }
