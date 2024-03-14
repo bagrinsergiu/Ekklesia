fc2de57a41b834728efb179650b48abacd00182b
DanielGherjavca <danniel.gherjavca@gmail.com>
2024-03-14 11:40:09 +0200

    fix(ministrybrands): event detail - button - order

--- src/Placeholder/EventDetailPlaceholder.php
+++ src/Placeholder/EventDetailPlaceholder.php
@@ -203,14 +203,14 @@ class EventDetailPlaceholder extends PlaceholderAbstract
                     echo "<div class=\"brz-eventDetail__item--meta--preview\"><span>{$item['text']}</span></div>";
                 }
 
+                if($show_subscribe_to_event ){
+                    echo '<div class="brz-eventDetail__item--subscribe-event"><a href="?mc-subscribe=' . $item["occurrenceid"] . '">' . $subscribe_to_event_button_text . '</a></div>';
+                }
+
                 if ($previous_page) {
                     echo '<div class="brz-ministryBrands__item--meta--links brz-ministryBrands__item--meta--links--previous">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512" class="brz-icon-svg align-[initial]"><path d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg>
                     Previous Page</div>';
-                }
-
-                if($show_subscribe_to_event ){
-                    echo '<div class="brz-eventDetail__item--subscribe-event"><a href="?mc-subscribe=' . $item["occurrenceid"] . '">' . $subscribe_to_event_button_text . '</a></div>';
                 }
 
                 ?>
