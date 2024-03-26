9eb62c64a8196214d6296b242b8f0178f3dd7dbf
Maxim Bondarenco <85360293+grandleemon@users.noreply.github.com>
2024-03-26 10:47:10 +0200

    changed search results classes (#120)

--- src/Placeholder/EventLayoutPlaceholder.php
+++ src/Placeholder/EventLayoutPlaceholder.php
@@ -441,7 +441,7 @@ class EventLayoutPlaceholder extends PlaceholderAbstract
         </div>
 
             <?php if (isset($_GET['mc-search'])) {
-                echo "<h4 class=\"ekklesia360_event_layout_results_heading\"><a href=\"{$baseURL}?mc-view=list\"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 352 512\" class=\"brz-icon-svg align-[initial]\" data-type=\"fa\" data-name=\"times\"><path d=\"M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z\"></path></svg></a> Search results for \"{$_GET['mc-search']}\"</h4>";
+                echo "<h5 class=\"brz-eventLayout-results-heading\"><a href=\"{$baseURL}?mc-view=list\"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 352 512\" class=\"brz-icon-svg align-[initial]\" data-type=\"fa\" data-name=\"times\"><path d=\"M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z\"></path></svg></a> Search results for \"{$_GET['mc-search']}\"</h5>";
             }
             ?>
         <?php endif; //end hide from featured
@@ -526,7 +526,7 @@ class EventLayoutPlaceholder extends PlaceholderAbstract
                 else {
                 ?>
 
-                    <p>There are no events available.</p>
+                    <p class="brz-eventLayout-no-results">There are no events available.</p>
 
                 <?php
                 }
@@ -557,7 +557,7 @@ class EventLayoutPlaceholder extends PlaceholderAbstract
                 else {
                 ?>
 
-                    <p>There are no events available.</p>
+                    <p class="brz-eventLayout-no-results">There are no events available.</p>
 
                 <?php
                 }
@@ -592,7 +592,7 @@ class EventLayoutPlaceholder extends PlaceholderAbstract
                 else {
                 ?>
 
-                    <p>There are no events available.</p>
+                    <p class="brz-eventLayout-no-results">There are no events available.</p>
 
                 <?php
                 }
--- src/Placeholder/SermonLayoutPlaceholder.php
+++ src/Placeholder/SermonLayoutPlaceholder.php
@@ -241,7 +241,7 @@ class SermonLayoutPlaceholder extends PlaceholderAbstract
 
         <?php
             if (isset($_GET['mc-search'])) {
-                echo "<h4 class=\"ekklesia360_sermon_layout_results_heading\"><a href=\"{$baseURL}\"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 352 512\" class=\"brz-icon-svg\" data-type=\"fa\" data-name=\"times\"><path d=\"M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z\"></path></svg></a> Search results for \"{$_GET['mc-search']}\"</h4>";
+                echo "<h5 class=\"brz-sermonLayout-results-heading\"><a href=\"{$baseURL}\"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 352 512\" class=\"brz-icon-svg\" data-type=\"fa\" data-name=\"times\"><path d=\"M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z\"></path></svg></a> Search results for \"{$_GET['mc-search']}\"</h5>";
             }
         ?>
 
@@ -393,7 +393,7 @@ class SermonLayoutPlaceholder extends PlaceholderAbstract
                     echo "</div>";
                 }
             } else {
-                echo '<p>There are no sermons available.</p>';
+                echo '<p class="brz-sermonLayout-no-results">There are no sermons available.</p>';
             }
             ?>
         </div>
