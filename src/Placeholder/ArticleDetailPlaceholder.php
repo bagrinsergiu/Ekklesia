a5e4ffc3049e4620413fc4578e555b89cdbeef00
Maxim Bondarenco <85360293+grandleemon@users.noreply.github.com>
2024-04-25 16:09:56 +0300

    additional meta classes for article detail (#145)

--- src/Placeholder/ArticleDetailPlaceholder.php
+++ src/Placeholder/ArticleDetailPlaceholder.php
@@ -64,11 +64,11 @@ class ArticleDetailPlaceholder extends PlaceholderAbstract
         echo "<div class=\"brz-articleDetail__item\">";
 
         if ($show_title) {
-            echo "<h2 class=\"brz-articleDetail__item--meta--title\">{$item['title']}</h2>";
+            echo "<h2 class=\"brz-articleDetail__item--meta brz-ministryBrands__item--meta-title\">{$item['title']}</h2>";
         }
 
         if ($show_date && $item['date']) {
-            echo "<h6 class=\"brz-articleDetail__item--meta\">";
+            echo "<h6 class=\"brz-articleDetail__item--meta brz-ministryBrands__item--meta-date\">";
             if ($show_meta_headings) {
                 if ($show_meta_icons) {
                     echo "<span class=\"brz-ministryBrands__meta--icons\"><svg class=\"brz-icon-svg align-[initial]\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 448 512\"><path fill=\"currentColor\" d=\"M96 32V64H48C21.5 64 0 85.5 0 112v48H448V112c0-26.5-21.5-48-48-48H352V32c0-17.7-14.3-32-32-32s-32 14.3-32 32V64H160V32c0-17.7-14.3-32-32-32S96 14.3 96 32zM448 192H0V464c0 26.5 21.5 48 48 48H400c26.5 0 48-21.5 48-48V192z\"></path></svg></span>";
@@ -81,7 +81,7 @@ class ArticleDetailPlaceholder extends PlaceholderAbstract
         }
 
         if ($show_category && $item['category']) {
-            echo "<h6 class=\"brz-articleDetail__item--meta\">";
+            echo "<h6 class=\"brz-articleDetail__item--meta brz-ministryBrands__item--meta-category\">";
             if ($show_meta_headings) {
                 if ($show_meta_icons) {
                     echo "<span class=\"brz-ministryBrands__meta--icons\"><svg class=\"brz-icon-svg align-[initial]\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 384 512\"><path fill=\"currentColor\" d=\"M0 48V487.7C0 501.1 10.9 512 24.3 512c5 0 9.9-1.5 14-4.4L192 400 345.7 507.6c4.1 2.9 9 4.4 14 4.4c13.4 0 24.3-10.9 24.3-24.3V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48z\"></path></svg></span>";
@@ -94,7 +94,7 @@ class ArticleDetailPlaceholder extends PlaceholderAbstract
         }
 
         if ($show_group && $item['group']) {
-            echo "<h6 class=\"brz-articleDetail__item--meta\">";
+            echo "<h6 class=\"brz-articleDetail__item--meta brz-ministryBrands__item--meta-group\">";
             if ($show_meta_headings) {
                 if ($show_meta_icons) {
                     echo "<span class=\"brz-ministryBrands__meta--icons\"><svg class=\"brz-icon-svg align-[initial]\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 640 512\"><path fill=\"currentColor\" d=\"M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192h42.7c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0H21.3C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7h42.7C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3H405.3zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352H378.7C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7H154.7c-14.7 0-26.7-11.9-26.7-26.7z\"></path></svg></span>";
@@ -107,7 +107,7 @@ class ArticleDetailPlaceholder extends PlaceholderAbstract
         }
 
         if ($show_series && $item['series']) {
-            echo "<h6 class=\"brz-articleDetail__item--meta\">";
+            echo "<h6 class=\"brz-articleDetail__item--meta brz-ministryBrands__item--meta-series\">";
             if ($show_meta_headings) {
                 if ($show_meta_icons) {
                     echo "<span class=\"brz-ministryBrands__meta--icons\"><svg class=\"brz-icon-svg align-[initial]\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\"><path d=\"M40 48C26.7 48 16 58.7 16 72v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V72c0-13.3-10.7-24-24-24H40zM192 64c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zM16 232v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V232c0-13.3-10.7-24-24-24H40c-13.3 0-24 10.7-24 24zM40 368c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V392c0-13.3-10.7-24-24-24H40z\"/></svg></span>";
@@ -120,7 +120,7 @@ class ArticleDetailPlaceholder extends PlaceholderAbstract
         }
 
         if ($show_author && $item['author']) {
-            echo "<h6 class=\"brz-articleDetail__item--meta\">";
+            echo "<h6 class=\"brz-articleDetail__item--meta brz-ministryBrands__item--meta-author\">";
             if ($show_meta_headings) {
                 if ($show_meta_icons) {
                     echo "<span class=\"brz-ministryBrands__meta--icons\"><svg class=\"brz-icon-svg align-[initial]\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 448 512\"><path d=\"M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z\"/></svg></span>";
