93b0289ece34c0419d6cd6ac383f77d1b48afe7f
Daniel Gherjavca <67125532+DanielGherjavca@users.noreply.github.com>
2024-04-29 13:40:06 +0300

    refactor(ministrybrands): staffLayout - changed params name (#146)

--- src/Placeholder/StaffLayoutPlaceholder.php
+++ src/Placeholder/StaffLayoutPlaceholder.php
@@ -100,7 +100,7 @@ class StaffLayoutPlaceholder extends PlaceholderAbstract
             <?php if ($show_search) : ?>
                 <form method="get" id="brz-staffLayout__filters--form-search" name="search" class="brz-staffLayout__filters--form-search" action="<?= $baseURL ?>" data-count="<?= $filterCount ?>">
                     <fieldset>
-                        <input type="text" id="brz-staffLayout__filters--form-search_term" name="mc-search" value="" placeholder="<?= $search_placeholder ?>" />
+                        <input type="text" id="brz-staffLayout__filters--form-search_term" name="mc-search_term" value="" placeholder="<?= $search_placeholder ?>" />
                         <button type="submit" id="brz-staffLayout__filters--form-search_submit"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="brz-icon-svg align-[initial]" data-type="fa" data-name="search">
                                 <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
                             </svg></button>
@@ -111,7 +111,7 @@ class StaffLayoutPlaceholder extends PlaceholderAbstract
 
         <?php
         if (isset($_GET['mc-search_term'])) {
-            echo "<h4 class=\"brz-staffLayout-results-heading\"><a href=\"{$baseURL}\"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 352 512\" class=\"brz-icon-svg align-[initial]\" data-type=\"fa\" data-name=\"times\"><path d=\"M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z\"></path></svg></a> Search results for \"{$_GET['mc-search']}\"</h4>";
+            echo "<h4 class=\"brz-staffLayout-results-heading\"><a href=\"{$baseURL}\"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 352 512\" class=\"brz-icon-svg align-[initial]\" data-type=\"fa\" data-name=\"times\"><path d=\"M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z\"></path></svg></a> Search results for \"{$_GET['mc-search_term']}\"</h4>";
         }
         ?>
         <div class="brz-staffLayout__container">
@@ -246,7 +246,7 @@ class StaffLayoutPlaceholder extends PlaceholderAbstract
                 </div>
             <?php } else { ?>
 
-                <p>There are no staff available.</p>
+                <p class="brz-staffLayout-no-results">There are no staff available.</p>
 
             <?php
             }
