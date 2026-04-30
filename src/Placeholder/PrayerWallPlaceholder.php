8055fb6266b7d55bdb6a6783242bf41d2e778840
Viorel <eremiaviorel0@gmail.com>
2026-04-30 16:21:31 +0300

    fix(prayer-placeholder): update endpoint URL to `/m-b/placeholder/render` for consistency

--- src/Placeholder/PrayerWallPlaceholder.php
+++ src/Placeholder/PrayerWallPlaceholder.php
@@ -30,7 +30,7 @@ class PrayerWallPlaceholder extends PlaceholderAbstract
     ) {
         parent::__construct($monkCMS, $twig, $replacer);
         $this->prayerCloudApi = $prayerCloudApi;
-        $this->endpointUrl    = '/mb/placeholder/render';
+        $this->endpointUrl    = '/m-b/placeholder/render';
     }
 
     private static function escapeHtml($text)
